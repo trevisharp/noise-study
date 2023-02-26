8ae9bf26d72576473d51fe1e3880613d2b44c781
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2023-02-26 05:49:45 -0300

    Lib folder reorganization

--- Signals/Program.cs
+++ Signals/Program.cs
@@ -1,44 +1,22 @@
 ﻿using System;
 using Signals.Internal;
 
-int N = 1024;
+int N = 65536;
+
+DateTime dt;
+TimeSpan span;
 
 float[] signal = new float[N];
 float[] isignal = new float[N];
+signal[1] = 5;
 
-for (int i = 0; i < N; i++)
+dt = DateTime.Now;
+for (int t = 0; t < 1000; t++)
 {
-    signal[i] = N * (5 + .5f * (2 * Random.Shared.NextSingle() - 1));
-    isignal[i] = N * (5 + .5f * (2 * Random.Shared.NextSingle() - 1));
+    FourrierTransform.FFT(signal, isignal);
 }
-
-FourrierTransform.IFFT(signal, isignal);
-
-foreach (var x in signal)
-{
-    var rounded = MathF.Round(x, 2);
-    if (rounded == 0)
-        rounded = 0f;
-    var str = rounded.ToString().Replace(',','.');
-    Console.Write($"{str}, ");
-}
-
-return;
-
-// DateTime dt;
-// TimeSpan span;
-
-// float[] signal = new float[N];
-// float[] isignal = new float[N];
-// signal[1] = 5;
-
-// dt = DateTime.Now;
-// for (int t = 0; t < 1000; t++)
-// {
-//     FourrierTransform.FFT(signal, isignal);
-// }
-// span = DateTime.Now - dt;
-// Console.WriteLine(span.TotalMilliseconds / 1000);
+span = DateTime.Now - dt;
+Console.WriteLine(span.TotalMilliseconds / 1000);
 
 // void testIDFT()
 // {
