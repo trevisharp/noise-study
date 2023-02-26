5faa56cf00647db5a420c9de7d604c2959960ac4
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2023-02-26 06:24:24 -0300

    Signarl Abstraction implementation

--- Signals/Program.cs
+++ Signals/Program.cs
@@ -1,92 +1,10 @@
 ﻿using System;
-using Signals.Internal;
+using Signals;
 
-int N = 65536;
+Console.WriteLine("Aplicação Teste");
 
-DateTime dt;
-TimeSpan span;
+var signal = Signal.Empty();
+signal.FFT();
+signal.IFFT();
 
-float[] signal = new float[N];
-float[] isignal = new float[N];
-signal[1] = 5;
-
-dt = DateTime.Now;
-for (int t = 0; t < 1000; t++)
-{
-    FourrierTransform.FFT(signal, isignal);
-}
-span = DateTime.Now - dt;
-Console.WriteLine(span.TotalMilliseconds / 1000);
-
-// void testIDFT()
-// {
-//     float[] signal = new float[N];
-//     float[] isignal = new float[N];
-//     signal[1] = 5;
-//     (signal, isignal) = FourrierTransform.DFT(signal, isignal);
-
-//     (signal, isignal) = FourrierTransform.IDFT(signal, isignal);
-
-//     foreach (var x in signal)
-//     {
-//         var rounded = MathF.Round(x, 2);
-//         if (rounded == 0)
-//             rounded = 0f;
-//         var str = rounded.ToString().Replace(',','.');
-//         Console.Write($"{str}, ");
-//     }
-// }
-
-// void testIFFT()
-// {
-//     float[] signal = new float[N];
-//     float[] isignal = new float[N];
-//     signal[1] = 5;
-//     FourrierTransform.FFT(signal, isignal);
-
-//     // (signal, isignal) = FourrierTransform.IDFT(signal, isignal);
-
-//     foreach (var x in signal)
-//     {
-//         var rounded = MathF.Round(x, 2);
-//         if (rounded == 0)
-//             rounded = 0f;
-//         var str = rounded.ToString().Replace(',','.');
-//         Console.Write($"{str}, ");
-//     }
-// }
-
-
-// void testFFT()
-// {
-//     float[] signal = new float[N];
-//     float[] isignal = new float[N];
-//     signal[1] = 5;
-//     FourrierTransform.FFT(signal, isignal);
-
-//     foreach (var x in signal)
-//     {
-//         var rounded = MathF.Round(x, 2);
-//         if (rounded == 0)
-//             rounded = 0f;
-//         var str = rounded.ToString().Replace(',','.');
-//         Console.Write($"{str}, ");
-//     }
-// }
-
-// void testDFT()
-// {
-//     float[] signal = new float[N];
-//     float[] isignal = new float[N];
-//     signal[1] = 5;
-//     (signal, isignal) = FourrierTransform.DFT(signal, isignal);
-
-//     foreach (var x in signal)
-//     {
-//         var rounded = MathF.Round(x, 2);
-//         if (rounded == 0)
-//             rounded = 0f;
-//         var str = rounded.ToString().Replace(',','.');
-//         Console.Write($"{str}, ");
-//     }
-// }
+Console.WriteLine("Teste completo");
--- Signals/Signal.cs
+++ Signals/Signal.cs
@@ -1,11 +1,69 @@
 using System;
-using System.Linq;
-using System.Drawing;
-using System.Threading.Tasks;
+using System.Buffers;
 
-namespace SignalLib;
+namespace Signals;
 
-public class Signal
+using Internal;
+
+public class Signal : IDisposable
 {
+    private float[] real;
+    private float[] imag;
+    private bool disposed = false;
+
+    private Signal(float[] real, float[] imag)
+    {
+        this.real = real;
+        this.imag = imag;
+    }
     
+    ~Signal()
+    {
+        if (disposed)
+            return;
+        
+        Dispose();
+    }
+
+    public Signal FFT()
+    {
+        FourrierTransform.FFT(this.real, this.imag);
+        return this;
+    }
+    
+    public Signal IFFT()
+    {
+        FourrierTransform.IFFT(this.real, this.imag);
+        return this;
+    }
+
+    public void Dispose()
+    {
+        if (disposed)
+            return;
+        
+        disposed = true;
+        pool.Return(this.real);
+        pool.Return(this.imag);
+    }
+
+    private static ArrayPool<float> pool;
+    
+    private static void initPool()
+    {
+        if (pool != null)
+            return;
+        
+        pool = ArrayPool<float>.Create(1024 * 1024, 1024);
+    }
+    
+    public static Signal Empty(int N = 1024)
+    {
+        initPool();
+
+        float[] real = pool.Rent(1024);
+        float[] imag = pool.Rent(1024);
+
+        return new Signal(real, imag);
+    }
 }
